dc94ccd01a6cbdc8af4a5d61ee3d3a71fe041fb8
Omicron <ajwilson.ninety9@gmail.com>
2025-09-18 21:37:22 +0800

    New files to generate shapes and generate the player shape. Added new parameters to constants.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from constants import *
+from player import *
 import pygame
 
 def main():
     pygame.init()
+    Clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(
         f"Screen width: {SCREEN_WIDTH} \n"
@@ -14,7 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0,))
+        player.draw(screen)
         pygame.display.flip()
+        dt = Clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
