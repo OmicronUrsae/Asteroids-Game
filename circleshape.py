d42d21cd8a46fdaa508e3b1706446e253313748b
Omicron <ajwilson.ninety9@gmail.com>
2025-09-24 23:30:47 +0800

    Created randomised asteroid spawn including size and velocity. WIP on creating player projectiles.

--- circleshape.py
+++ circleshape.py
@@ -1,4 +1,5 @@
 import pygame
+from constants import *
 
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
@@ -16,3 +17,10 @@ class CircleShape(pygame.sprite.Sprite):
 
     def update(self, dt):
         pass
+
+    def collision_check(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
+    
+
+
+    
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from constants import *
 from player import *
+from asteroids import *
+from asteroidfield import *
+from Shot import *
 import pygame
 
 def main():
@@ -7,6 +10,15 @@ def main():
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    projectiles = pygame.sprite.Group()
+    Shot.containers = (updatable, drawable, projectiles)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting Asteroids!")
     print(
@@ -17,8 +29,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if player.collision_check(asteroid):
+                print("Game over!")
+                pygame.quit()
+                return
         screen.fill((0,0,0,))
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000.0
 
